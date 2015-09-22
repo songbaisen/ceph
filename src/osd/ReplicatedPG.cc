16b25e184a1d37c368471494438f306cacfb4bfd
Jianpeng Ma <jianpeng.ma@intel.com>
2015-09-22 11:56:13 +0800

    osd/ReplicatedPG: for omapsetheader, remove the duplicated code.
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -5618,7 +5618,6 @@ int ReplicatedPG::do_osd_ops(OpContext *ctx, vector<OSDOp>& ops)
 	ctx->delta_stats.num_wr++;
       }
       obs.oi.set_flag(object_info_t::FLAG_OMAP);
-      obs.oi.clear_omap_digest();
       break;
 
     case CEPH_OSD_OP_OMAPCLEAR:
